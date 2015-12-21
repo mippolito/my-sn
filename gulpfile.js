590c0f7e7828e2c897d00ebb337acd5d5b62b5eb
Marcelo Ippolitoh <marceloippolito@hotmail.com>
2015-12-21 01:14:58 -0300

    remove node_modules

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,24 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var browserify = require('gulp-browserify');
+var Ractive = require('ractive');
+var tap = require('gulp-tap');
+
+gulp.task('templates',function(){
+	gulp.src('./tpl/**/*.html')
+	.pipe(tap(function(file, t){
+		var precompiled = Ractive.parse(file.contents.toString());
+		precompiled = JSON.stringify(precompiled);
+		file.contents = new Buffer('module.exports = ' + precompiled);
+	}))
+	.pipe(rename(function(path) {
+		path.extname = '.js';
+	}))
+	.pipe(gulp.dest('./tpl'))
+});
 
 gulp.task('css',function(){
 	gulp.src('./less/styles.less')
@@ -14,8 +32,17 @@ gulp.task('css',function(){
 	.pipe(gulp.dest('./static/css'));
 });
 
+gulp.task('js', function() {
+	gulp.src('./js/app.js')
+	.pipe(browserify())
+	.pipe(gulp.dest('./static/js'))
+	.pipe(uglify())
+	.pipe(rename({suffix:'.min'}))
+	.pipe(gulp.dest('./static/js'))
+});
+
 gulp.task('watchers', function() {
 	gulp.watch('less/**/*.less', ['css']);
 });
 
-gulp.task('default', ['css', 'watchers']);
+gulp.task('default', ['css', 'templates', 'js', 'watchers']);
