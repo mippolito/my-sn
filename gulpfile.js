12267ff60de4071a5f05ad6b9a2e073431c05619
Marcelo Ippolito <marcelo.ippolito.pd@gmail.com>
2015-12-20 21:58:39 -0300

    fist commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var rename = require('gulp-rename');
+var minifyCSS = require('gulp-minify-css');
+
+gulp.task('css',function(){
+	gulp.src('./less/styles.less')
+	.pipe(less({ path: [path.join(__dirname, 'less', 'includes')]
+	}))
+	.pipe(gulp.dest('./static/css'))
+	.pipe(minifyCSS({keepBreaks:true}))
+	.pipe(rename({suffix:'.min'}))
+	.pipe(gulp.dest('./static/css'));
+});
+
+gulp.task('watchers', function() {
+	gulp.watch('less/**/*.less', ['css']);
+});
+
+gulp.task('default', ['css', 'watchers']);
